a194175644f051d572bf5efc494050d60165a59e
somnoynadno <somnoynadno@yandex.ru>
2020-05-10 17:40:11 +0700

    Calculating distribution parameters

--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,20 @@ google.charts.load('current', {packages: ['corechart']});
 // python-like
 let zip = (...rows) => [...rows[0]].map((_,c) => rows.map(row => row[c]));
 
+// chi-sqeare table
+var criticalValues = {
+  1:  3.841,
+  2:  5.991,
+  3:  7.815,
+  4:  9.488,
+  5:  11.070,
+  6:  12.592,
+  7:  14.067,
+  8:  15.507,
+  9:  16.919,
+  10: 18.301
+}
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
@@ -26,12 +40,19 @@ var db = new DB();
 
 class Simulator {
   constructor() {
-    this.probs   = [];
-    this.labels  = [];
-    this.events  = [];
+    this.probs      = [];
+    this.labels     = [];
+    this.events     = [];
+    this.exactProbs = [];
 
     this.counter = 0;
     this.N = 0;
+
+    this.E = 0;
+    this.D = 0;
+    this.exactE = 0;
+    this.exactD = 0;
+    this.chi = 0;
   }
 
   addRow() {
@@ -62,7 +83,14 @@ class Simulator {
       this._extractLabels();
       this._extractN();
 
+      this._countDistributionParameters();
+
       this._simulateEvents();
+      this._countExactProbs();
+      this._countExactDistributionParameters();
+
+      this._appendProbsToLabels();
+      this._setResultValues();
       this._drawResult();
     }
   }
@@ -82,6 +110,61 @@ class Simulator {
         }
       }
     }
+  }
+
+  _countExactProbs() {
+    for (let i = 0; i < this.events.length; i++) {
+      this.exactProbs.push(this.events[i]/this.N);
+    }
+  }
+
+  _countDistributionParameters() {
+    for (let i = 0; i < this.probs.length; i++) {
+      this.E += this.labels[i] * this.probs[i];
+    }
+
+    for (let i = 0; i < this.probs.length; i++) {
+      this.D += this.probs[i] * (this.labels[i] - this.E) * (this.labels[i] - this.E);
+    }
+  }
+
+  _countExactDistributionParameters() {
+    for (let i = 0; i < this.exactProbs.length; i++) {
+      this.exactE += this.labels[i] * this.exactProbs[i];
+    }
+
+    for (let i = 0; i < this.exactProbs.length; i++) {
+      this.exactD += this.exactProbs[i] * (this.labels[i] - this.exactE) * (this.labels[i] - this.exactE);
+    }
+
+    for (let i = 0; i < this.probs.length; i++) {
+      this.chi += (this.events[i] * this.events[i])/(this.N * this.probs[i]);
+    }
+    this.chi -= this.N;
+  }
+
+  _appendProbsToLabels() {
+    for (let i = 0; i < this.exactProbs.length; i++) {
+      // appending to the end of the label
+      let proba = " (" + (this.exactProbs[i]*100).toFixed(1)  + "%)";
+      this.labels[i] += proba;
+    }
+  }
+
+  _setResultValues() {
+    let dD = Math.abs(this.exactD - this.D) / Math.abs(this.D);
+    let dE = Math.abs(this.exactE - this.E) / Math.abs(this.E);
+
+    let text = "";
+    if (this.chi < criticalValues[this.counter]) {
+      text = this.chi.toFixed(3) + " &lt; " + criticalValues[this.counter] + ' (\<span class="text-success">не отклоняется</span>)';
+    } else {
+      text = this.chi.toFixed(3) + " &gt; " + criticalValues[this.counter] + ' (<span class="text-danger">не принимается</span>)';
+    }
+
+    $("#average").html("Матожидание: " + this.E.toFixed(3) + " (погрешность " + (dE*100).toFixed(1) + "%)");
+    $("#variance").html("Дисперсия: " + this.D.toFixed(3) + " (погрешность " + (dD*100).toFixed(1) + "%)");
+    $("#chi-square").html("Хи-квадрат: " + text);
   }
 
   _drawResult() {
@@ -116,9 +199,9 @@ class Simulator {
 
   _extractLabels() {
     for (let i = 0; i < this.counter; i++) {
-      let v = document.getElementById('name' + i).value;
-      if (v == "") {
-        this._panic("Empty label for row " + (i+1));
+      let v = parseFloat(document.getElementById('name' + i).value);
+      if (isNaN(v)) {
+        this._panic("Value for row "  + (i+1) + " must be float");
       }
       this.labels.push(v);
     }
@@ -137,11 +220,17 @@ class Simulator {
   }
 
   _cleanup() {
-    this.probs  = [];
-    this.labels = [];
-    this.events = [];
+    this.probs      = [];
+    this.exactProbs = [];
+    this.labels     = [];
+    this.events     = [];
 
     this.N = 0;
+    this.E = 0;
+    this.D = 0;
+    this.chi = 0;
+    this.exactE = 0;
+    this.exactD = 0;
   }
 }
 
